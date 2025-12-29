cc6c78e714e9b8206d888073514d8198e5f55cc5
ChuanyiZ <chuanyi5@illinois.edu>
2025-12-29 00:43:29 -0500

    feat: update base URL in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://ChuanyiZ.github.io/larp-wuxi',
   plugins: [
     vue(),
     vueDevTools(),
