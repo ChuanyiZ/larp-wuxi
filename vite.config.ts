8d0a4502eee6c6136fdc1924f714da36606b38a1
ChuanyiZ <chuanyi5@illinois.edu>
2025-12-29 01:06:15 -0500

    fix: base URL in vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://ChuanyiZ.github.io/larp-wuxi',
+  base: '/larp-wuxi/',
   plugins: [
     vue(),
     vueDevTools(),
